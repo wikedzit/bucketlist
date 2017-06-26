a88959202e66d47f032797c2c5790461fe458392
wikedzit <timothy.wikedzi@andela.com>
2017-06-26 10:15:32 +0300

    add tests boilerplates

---
+++ api/v1/tests/test_api_tokens.py
@@ -0,0 +1,21 @@
+import unittest
+import json
+
+class TestAuthentication(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_validates_user_inputs(self):
+        self.assertTrue(True)
+
+    def test_user_can_register(self):
+        self.assertTrue(True)
+
+    def test_no_ducplicated_usernames(self):
+        self.assertTrue(True)
+
+    def test_user_can_login(self):
+        self.assertTrue(True)
