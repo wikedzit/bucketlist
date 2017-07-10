953b192ae0b9c02de6364a807f1a824e5e8977fe
wikedzit <timothy.wikedzi@andela.com>
2017-07-10 06:48:27 +0300

    add manual DB Management functionalities

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from sqlalchemy_utils.functions import drop_database
+from sqlalchemy_utils.functions import create_database, drop_database
 from api.v1.app import app, databases
 
 migrate = Migrate(app, databases)
 manager = Manager(app)
 manager.add_command('databases', MigrateCommand)
 
+
+@manager.command
+def createdb():
+    create_database(app.config['SQLALCHEMY_DATABASE_URI'])
 
 @manager.command
 def dropdb():
